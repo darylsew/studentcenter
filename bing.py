96cbca9d9ce6546d0967d45f47156af2e2723079
Daryl Sew <darylsew@gmail.com>
2014-11-19 10:13:13 -0500

    add nclasses support

--- bing.py
+++ bing.py
@@ -7,8 +7,7 @@ username = f[0]
 password = f[1]
 successes = 0
 failures = 0
-class1 = 25225
-class2 = 25383
+classes = [25225, 25383, 22868, 27916]
 
 def find_by_tag_val(tag, val):
     ls = driver.find_elements_by_tag_name(tag)
@@ -25,10 +24,12 @@ while True:
         driver.get("https://ssb.cc.binghamton.edu/banner/bwskfreg.P_AltPin")
         submit = find_by_tag_val("input", "Submit")
         submit.click()
-        first = driver.find_element_by_id("crn_id1")
-        first.send_keys(class1)
-        second = driver.find_element_by_id("crn_id2")
-        second.send_keys(class2)
+
+        # Actually inputting classes
+        for i, cl in enumerate(classes):
+            field = driver.find_element_by_id("crn_id" + str(i+1))
+            field.send_keys(cl)
+
         submit = find_by_tag_val("input", "Submit Changes")
         submit.click()
         driver.close()
@@ -39,4 +40,4 @@ while True:
         failures += 1
         print("failures: " + str(failures))
         driver.close()
-    time.sleep(10)
+    time.sleep(300)
