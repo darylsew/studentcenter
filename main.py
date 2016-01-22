16e3cddacb8018627cc1401c83fedb82efe2b194
Daryl Sew <darylsew@gmail.com>
2016-01-21 20:13:26 -0500

    update for new student center

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 f = [i.strip() for i in open('creds').readlines()]
 username = f[0]
@@ -15,23 +16,17 @@ while True:
         netidinput.send_keys(username)
         passwdinput.send_keys(password)
         driver.find_element_by_name("Submit").click()
-        driver.find_element_by_id("DERIVED_SSS_SCL_LINK_ADD_ENRL").click()
-        semesters = driver.find_elements_by_name("SSR_DUMMY_RECV1$sels$0")
-        semesters[1].click()
-        driver.find_element_by_name("DERIVED_SSS_SCT_SSR_PB_GO").click()
-        
-        # Code for searching (no need to repeat; student center caches classes in cart)
-        #driver.find_element_by_name("DERIVED_REGFRM1_CLASS_NBR$42$").send_keys(int(lect))
-        #driver.find_element_by_id("DERIVED_REGFRM2_SSR_PB_ADDTOLIST2$44$").click()
-        #discussions = driver.find_elements_by_name("SSR_CLS_TBL_RE$sels$0")
-        #discussions[0].click()
-        #driver.find_element_by_id("DERIVED_CLS_DTL_NEXT_PB").click()
-        #driver.find_element_by_id("DERIVED_CLS_DTL_NEXT_PB").click()
-        driver.find_element_by_id("DERIVED_REGFRM1_LINK_ADD_ENRL").click()
+        time.sleep(2)
+        driver.find_element_by_id("DERIVED_SSS_SCR_SSS_LINK_ANCHOR3").click()
+        time.sleep(2)
+        driver.find_element_by_id("DERIVED_REGFRM1_LINK_ADD_ENRL$82$").click()
+        time.sleep(2)
         driver.find_element_by_id("DERIVED_REGFRM1_SSR_PB_SUBMIT").click()
-        driver.close()
         successes += 1
         print("successes: " + str(successes))
+        # Try every 100 seconds
+        time.sleep(100)
+        driver.close()
     except:
         # Non-deterministic html data...
         failures += 1
