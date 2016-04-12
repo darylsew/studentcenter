0e820160b87eb3b4d5d2b952281c664969abf890
Daryl Sew <darylsew@gmail.com>
2016-04-12 12:45:12 -0400

    phantomjs cleanup

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import os
 
 f = [i.strip() for i in open('creds').readlines()]
 username = f[0]
 password = f[1]
 successes = 0
 failures = 0
+
+NETWORK_SLOWNESS = 3
 while True:
     try:
         driver = webdriver.PhantomJS()
@@ -16,23 +19,25 @@ while True:
         netidinput.send_keys(username)
         passwdinput.send_keys(password)
         driver.find_element_by_name("Submit").click()
-        time.sleep(2)
+        time.sleep(NETWORK_SLOWNESS)
         driver.find_element_by_id("DERIVED_SSS_SCR_SSS_LINK_ANCHOR3").click()
-        time.sleep(2)
+        time.sleep(NETWORK_SLOWNESS)
         driver.find_element_by_id("SSR_DUMMY_RECV1$sels$2$$0").click()
-        time.sleep(2)
+        time.sleep(NETWORK_SLOWNESS)
         driver.find_element_by_id("DERIVED_SSS_SCT_SSR_PB_GO").click()
-        time.sleep(2)
+        time.sleep(NETWORK_SLOWNESS)
         driver.find_element_by_id("DERIVED_REGFRM1_LINK_ADD_ENRL$82$").click()
-        time.sleep(2)
+        time.sleep(NETWORK_SLOWNESS)
         driver.find_element_by_id("DERIVED_REGFRM1_SSR_PB_SUBMIT").click()
         successes += 1
         print("successes: " + str(successes))
         # Try every 100 seconds
         time.sleep(100)
         driver.close()
+        os.system("pkill -9 phantomjs")
     except:
-        # Non-deterministic html data...
+        # Deal w non-deterministic html data...
+        # or network issues or whatever
         failures += 1
         print("failures: " + str(failures))
         driver.close()
